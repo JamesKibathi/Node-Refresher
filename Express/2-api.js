3b4c4fd08e56588cdcc26a38eabaaefa33b5dfb0
James Njenga <knjenga94@gmail.com>
2023-03-05 00:06:52 +0300

    Added string query params

--- Express/2-api.js
+++ Express/2-api.js
@@ -28,6 +28,30 @@ app.get("/api/products/:productID",(req,res)=>{
     return res.json(singleProduct)
 })
 
+// search query string params - http://localhost:5000/api/v1/query?search=n
+
+app.get("/api/v1/query",(req,res)=>{
+    //console.log(req.query)
+    const {search,limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts=sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+
+    if(limit){
+        sortedProducts=sortedProducts.slice(0,Number(limit))
+    }
+
+    if(sortedProducts.length<1){
+        // res.status(200).send("no products matched your search")
+        return res.status(200).json({success:true,data:[]})
+    }
+    res.status(200).json(sortedProducts)
+})
+
 app.listen(5000,()=>{
     console.log("App listening on port 5000")
 })
